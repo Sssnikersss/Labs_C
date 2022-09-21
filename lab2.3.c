02372b1d978a7518a760fff38cccc4bc287f4e1e
Дмитрий <113302497+Sssnikersss@users.noreply.github.com>
2022-09-21 20:35:04 +0300

    Update lab2.3.c

--- lab2.3.c
+++ lab2.3.c
@@ -1,9 +1,9 @@
-//Вариант 2; n=3; k=4; m=2
+//Р’Р°СЂРёР°РЅС‚ 2; n=3; k=4; m=2
 #include<stdio.h>
 #include<locale.h>
 void main()
 {
 	setlocale(LC_ALL, "RUS");
 	int n = 3;
-	printf("\t\t\t\t\t\t\t\t\t\t\t\tДано: \tn=3; n/333\n\nОтвет:\t%+08.2f", n / 333.);
+	printf("\t\t\t\t\t\t\t\t\t\t\t\tР”Р°РЅРѕ: \tn=3; n/333\n\nРћС‚РІРµС‚:\t%+08.2f", n / 333.);
 }
