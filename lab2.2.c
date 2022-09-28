ad90a559674fd620b37aa99ca1286cb49a5aa9c4
Дмитрий <113302497+Sssnikersss@users.noreply.github.com>
2022-09-28 20:15:15 +0300

    Update lab2.2.c

--- lab2.2.c
+++ lab2.2.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<locale.h>
+
+
 void main()
 {
 	setlocale(LC_ALL, "RUS");
 	int N = 16, K = 25;
+	
 	printf("Сейчас %d часов %d минут 00 секунд\n",N,K);
 	printf("Идёт %d минута суток\n",N*60+K);
 	printf("До полуночи осталось %d часов и %d минут\n",24-N-1,60-K);
