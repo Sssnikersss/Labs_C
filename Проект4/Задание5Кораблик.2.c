5a0104fe8da11f750ee08d106054e263aa5cee89
Дмитрий <113302497+Sssnikersss@users.noreply.github.com>
2022-09-30 21:59:05 +0300

    Create Задание5Кораблик.2.c

---
+++ Проект4/Задание5Кораблик.2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+void main()
+{
+	printf("\t\t\t\t\t\t\t*\n");
+	printf("\t\t\t\t\t\t\t**\n");
+	printf("\t\t\t\t\t\t\t* *\n");
+	printf("\t\t\t\t\t\t\t*  *\n");
+	printf("\t\t\t\t\t\t\t*   *\n");
+	printf("\t\t\t\t\t\t\t*    *\n");
+	printf("\t\t\t\t\t\t\t*     *\n");
+	printf("\t\t\t\t\t\t\t*      *\n");
+	printf("\t\t\t\t\t\t\t*       *\n");
+	printf("\t\t\t\t\t\t\t*        *\n");
+	printf("\t\t\t\t\t\t\t*         *\n");
+	printf("\t\t\t\t\t\t\t************\n");
+	printf("\t\t\t\t\t\t\t*\n");
+	printf("\t\t\t\t\t\t\t*\n");
+	printf("\t\t\t\t\t\t\t*                  *\n");
+	printf("\t\t\t\t\t\t\t*                *****\n");
+	printf("\t\t\t\t\t\t\t*                  * \n");
+	printf("\t\t\t\t\t\t\t*                 * *\n");
+	printf("\t\t\t*******************************************************************\n");
+	printf("\t\t\t *                                                               *\n");
+	printf("\t\t\t  *       ******     ******     ******     ******     ******    *\n");
+	printf("\t\t\t   *      *    *     *    *     *    *     *    *     *    *   *\n");
+	printf("\t\t\t    *     ******     ******     ******     ******     ******  *\n");
+	printf("\t\t\t     *                                                       *\n");
+	printf("\t\t\t      *                                                     *\n");
+	printf("\t\t\t       *      *********       *********       *********    *\n");
+	printf("\t\t\t        *      *     *         *     *         *     *    *\n");
+	printf("\t\t\t         *      *****           *****           *****    *\n");
+	printf("\t\t\t         *                                               *\n");
+	printf("\t\t\t         *************************************************\n");
+}
